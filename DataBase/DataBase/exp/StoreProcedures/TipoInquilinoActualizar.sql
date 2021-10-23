d7eea2991cf7b8d12f9c399fff102e7e9afbb77e
Nohelia Sobalvarro <arial8520@gmail.com>
2021-10-23 15:12:01 -0600

    Commit WebApp

---
+++ DataBase/DataBase/exp/StoreProcedures/TipoInquilinoActualizar.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [EXP].[TipoInquilinoActualizar]
+    @Id_TipoInquilino int,
+	@Descripcion varchar(250),
+	@Estado bit
+
+AS BEGIN
+SET NOCOUNT ON
+
+  BEGIN TRANSACTION TRASA
+
+    BEGIN TRY
+	
+	UPDATE exp.TipoInquilino SET
+	Descripcion= @Descripcion,
+	 Estado=@Estado
+	 
+	WHERE 
+	       Id_TipoInquilino=Id_TipoInquilino
+	
+	  COMMIT TRANSACTION TRASA
+	  SELECT 0 AS CodeError, '' AS MsgError
+
+  END TRY
+
+  BEGIN CATCH
+
+	   SELECT 
+			 ERROR_NUMBER() AS CodeError,
+			 ERROR_MESSAGE() AS MsgError
+   
+	   ROLLBACK TRANSACTION TRASA
+
+   END CATCH
+
+ END
