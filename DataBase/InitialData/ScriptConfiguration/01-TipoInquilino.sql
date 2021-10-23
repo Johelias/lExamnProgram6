cd074c3c52c5f688c995aab7fc2066179d5a3de7
Nohelia Sobalvarro <arial8520@gmail.com>
2021-10-23 14:50:58 -0600

    Commit BD

---
+++ DataBase/InitialData/ScriptConfiguration/01-TipoInquilino.sql
@@ -0,0 +1,43 @@
+﻿DROP TABLE IF EXISTS #TipoInquilinoTemp
+
+SELECT
+Id_TipoInquilino, Descripcion, Estado INTO #TipoInquilinoTemp
+FROM (
+VALUES 
+(1,'JURIDICO',1),
+(2,'FISICO',1)
+
+)AS TEMP (Id_TipoInquilino, Descripcion, Estado)
+
+---------Actualizar Datos-------------
+
+
+	UPDATE T SET 
+		T.Descripcion=TM.Descripcion
+	FROM [dbo].[TipoInquilino] T
+	INNER JOIN #TipoInquilinoTemp TM
+	ON T.Id_TipoInquilino= TM.Id_TipoInquilino
+
+
+
+	---------Insertar Datos-------
+
+SET IDENTITY_INSERT dbo.TipoInquilino ON
+
+INSERT INTO  [dbo].[TipoInquilino](
+Id_TipoInquilino, Descripcion, Estado)
+SELECT
+Id_TipoInquilino, Descripcion, Estado
+FROM #TipoInquilinoTemp
+
+
+EXCEPT
+SELECT
+Id_TipoInquilino, Descripcion, Estado
+FROM dbo.TipoInquilino
+
+
+SET IDENTITY_INSERT dbo.TipoInquilino OFF
+
+GO
+
